983ef8e88bd9f7f9c6e5ca0411dd7018bdcded2a
Iya iya <rafiqmarwan100@gmail.com>
2025-10-15 20:02:56 +0700

    Cancel add 404.html, instead it becomes a bug

--- index.js
+++ index.js
@@ -13,10 +13,6 @@ const PORT = process.env.PORT || 3000;
 
 app.use(express.static(path.join(__dirname)));
 
-app.use((req, res) => {
-  res.status(404).sendFile(path.join(__dirname, '404.html'));
-});
-
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'index.html'));
 });
