f8024b2f58b51c7a8552090d7fef7b0421ee8f8e
Iya iya <rafiqmarwan100@gmail.com>
2025-12-24 14:02:39 +0700

    Add header for fetch pixeldrain API

--- index.js
+++ index.js
@@ -70,7 +70,7 @@ router.get('/downloader/pixeldrain', async (req, res) => {
     const fileId = url.split('/').pop();
     const response = await fetch(`https://pixeldrain.com/api/file/${fileId}/info`, {
   headers: {
-    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/143.0.0.0 Safari/537.36' 
+    'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/143.0.0.0 Safari/537.36' 
   }
 });
     const data = await response.json(); 
