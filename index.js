b9d0089237a811d5c952233b386a431fd793fb1d
Iya iya <rafiqmarwan100@gmail.com>
2025-12-24 14:01:19 +0700

    Update index.js

--- index.js
+++ index.js
@@ -68,7 +68,12 @@ router.get('/downloader/pixeldrain', async (req, res) => {
   if (!url) return res.status(400).json({ error: "Missing 'url' parameter" }); 
   try {
     const fileId = url.split('/').pop();
-    const data = await (await fetch(`https://pixeldrain.com/api/file/${fileId}/info`)).json(); 
+    const response = await fetch(`https://pixeldrain.com/api/file/${fileId}/info`, {
+  headers: {
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/143.0.0.0 Safari/537.36' 
+  }
+});
+    const data = await response.json(); 
     if (!data.name) {
       return res.status(404).json({ error: "Pixeldrain file not found or invalid title" });
     }  
