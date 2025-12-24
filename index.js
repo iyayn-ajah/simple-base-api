d0bc0d97a8be9643b73ea6b1d1b53256f43e49b1
Iya iya <rafiqmarwan100@gmail.com>
2025-12-24 14:04:39 +0700

    Fix syntax error

--- index.js
+++ index.js
@@ -69,10 +69,10 @@ router.get('/downloader/pixeldrain', async (req, res) => {
   try {
     const fileId = url.split('/').pop();
     const response = await fetch(`https://pixeldrain.com/api/file/${fileId}/info`, {
-  headers: {
-    'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/143.0.0.0 Safari/537.36' 
-  }
-});
+      headers: {
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/143.0.0.0 Safari/537.36' 
+      }
+    });
     const data = await response.json(); 
     if (!data.name) {
       return res.status(404).json({ error: "Pixeldrain file not found or invalid title" });
